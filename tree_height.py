7c1492445d67e9c36e3af4e10c994a180db1138e
ArtemijsRadionovs <124582137+ArtemijsRadionovs@users.noreply.github.com>
2023-03-01 03:32:19 +0200

    Second

--- tree_height.py
+++ tree_height.py
@@ -15,9 +15,13 @@ def compute_height(n, parents):
         node_height = 0
         parent = parents[i]
         while parent != - 1:
+            if heights[i]:
+                node_height += heights[i]
+                break
             node_height += 1
             parent = parents[parent]
-    max_height = max(heights)
+        heights[i] = node_height
+        max_height = max(max_height, node_height)
     
     return max_height
 
@@ -36,17 +40,19 @@ def main():
         file_name = input().strip()
         if 'a' in file_name:
             print("File name cannot contain letter 'a'!")
+            return
         try:
             with open('test/' + file_name, 'r') as f:
                 n = int(f.readline())
                 parents = list(map(int, f.readline().split()))
         except FileNotFoundError:
             print("File not found!")
+            return
     
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    
+    print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
