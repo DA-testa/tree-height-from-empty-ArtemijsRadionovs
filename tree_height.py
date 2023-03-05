38b589087d0cc40422c9b1d401e4d8b73f74b10a
ArtemijsRadionovs <124582137+ArtemijsRadionovs@users.noreply.github.com>
2023-03-05 17:52:47 +0000

    Final6

--- tree_height.py
+++ tree_height.py
@@ -27,53 +27,24 @@ def main():
         if n < parents.size:
             print("The number of child offspring is higher than it should be!")
             return
-        y = 3
-        print(y)
-        # print(parseTree(parents, "-1"))
+        print(parseTree(parents, "-1"))
         
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision    
         
     elif check_for_I == "F":
-        file_name = input().strip()
+        file_name = input().replace('\r','')
         if 'a' in file_name:
             print("File name cannot contain letter 'a'!")
             return
         try:
             with open('./test/' + file_name, 'r') as f:
                 n = int(f.readline())
-                parents = list(map(int, f.readline().split()))
+                parents = np.array(f.readline().split())
                 print(parseTree(parents, "-1"))
         except FileNotFoundError:
             print("File not found!")
             return
-
-
-
-# def compute_height(n, parents):
-# def compute_height(n, parents):
-#     # Write this function
-#     heights = [0]*n
-    
-#     max_height = 0
-#     # Your code here
-#     for i in range(n):
-#         node_height = 0
-#         parent = parents[i]
-#         while parent != - 1:
-#             if heights[i]:
-#                 node_height += heights[i]
-#                 break
-#             node_height += 1
-#             parent = parents[parent]
-#         heights[i] = node_height
-#         max_height = max(max_height, node_height)
-    
-#     return max_height
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-#     print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
